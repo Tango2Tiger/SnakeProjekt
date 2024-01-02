a7d138e46c8375e4184e044d60208132bb16bbf4
Holger <154835977+Holger02@users.noreply.github.com>
2024-01-02 16:27:09 +0100

    Add files via upload
    This is a snake file

---
+++ Snakeproject/Snake/Snake.java
@@ -0,0 +1,7 @@
+public class Snake{
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            System.out.println("Magnus skylder fanum tax, og flytter til skibbity ohio som rizzler");
+        }
+    }
+}
