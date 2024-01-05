e419ce4cf1c6a79a26908a213deb733b7b099b9c
Jonas Leth Grønbeck <j.leth.groenbeck@gmail.com>
2024-01-05 14:33:41 +0100

    nyt æble

---
+++ Snakeproject/Snake/newApple.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class NewApple() {
+    Random random = new Random();
+    int appleX, appleY;
+
+    appleX = random.nextInt(GamePanel.GRID_WIDTH);
+    appleY = random.nextInt(GamePanel.GRID_HEIGHT);
+    
+    
+}
