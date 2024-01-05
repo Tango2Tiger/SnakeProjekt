e693037d19cb1a04dcdeca262c4cff01af204cd3
Clement Koefoed <clementkrasmussen@gmail.com>
2024-01-05 15:16:27 +0100

    Removed folders and added color to snake

--- GamePanel.java
+++ GamePanel.java
@@ -12,10 +12,10 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GamePanel extends Application {
-    private GridPane grid;
-    private static final int GRID_HEIGHT = 20;
-    private static final int GRID_WIDTH = 35;
-    private static final int TILE_SIZE = 25;
+    public GridPane grid;
+    public static final int GRID_HEIGHT = 20;
+    public static final int GRID_WIDTH = 35;
+    public static final int TILE_SIZE = 25;
 
     public static void main(String[] args) {
         launch(args);
