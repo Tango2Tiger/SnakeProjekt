19ac3a5cb4c126df4722fbeed8d219c6375be773
Clement Koefoed <clementkrasmussen@gmail.com>
2024-01-05 14:43:48 +0100

    Snake constructor 1

--- Snakeproject/Snake/Snake.java
+++ Snakeproject/Snake/Snake.java
@@ -1,3 +1,24 @@
+
+import java.util.ArrayList;
+import javafx.scene.shape.Rectangle;
+
+
 public class Snake{
-    public static void main(String[] args) {
-        //This is our main class where we will initialize all our classes
+    private ArrayList<Rectangle> segments;
+
+    public Snake(int x, int y, int gridsize){
+        this.segments = new ArrayList<Rectangle>();
+        Rectangle head = new Rectangle(gridsize,gridsize);
+        head.setX(x);
+        head.setY(y);
+        segments.add(head);
+
+        Rectangle tail = new Rectangle(gridsize,gridsize);
+        head.setX(x+gridsize);
+        head.setY(y);
+        segments.add(tail);
+
+    }
+
+
+}
