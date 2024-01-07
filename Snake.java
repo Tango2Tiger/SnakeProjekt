02f5142266355e8a7b4abda394fbb8e707edd081
Clement Koefoed <clementkrasmussen@gmail.com>
2024-01-07 12:33:39 +0100

    Deleted newApple, edits to snake. New Branch

--- Snake.java
+++ Snake.java
@@ -11,17 +11,21 @@ public class Snake{
     public Snake(int x, int y){
         this.segments = new ArrayList<Rectangle>();
         Rectangle head = new Rectangle(GamePanel.TILE_SIZE,GamePanel.TILE_SIZE);
-        head.setFill(Color.BLACK);
+        head.setFill(Color.CORAL);
         head.setX(x);
         head.setY(y);
         segments.add(head);
 
         Rectangle tail = new Rectangle(GamePanel.TILE_SIZE,GamePanel.TILE_SIZE);
         tail.setFill(Color.LIMEGREEN);
-        tail.setX(x+GamePanel.TILE_SIZE);
+        tail.setX(x+1);
         tail.setY(y);
         segments.add(tail);
 
+    }
+
+    public ArrayList getlist(){
+        return this.segments;
     }
 
 
